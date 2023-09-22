a3ab41c834af6cea36c049c6b606ae67e3c05296
jajalovack <dvtejuco@gmail.com>
2023-09-22 15:03:19 +0800

    added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,11 @@
+import myLogo from "../assets/myLogo-cropped.svg"
+import "./Footer.scoped.css"
+
 const Footer = () => {
   return (
     <footer>
-        
+        <object data={myLogo}></object><br/>
+        &copy;Daniele Victor Tejuco, 2023
     </footer>
   )
 }
